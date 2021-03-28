1d085bdda1024b2c37b5eb09fa8471d315c83dac
Thanusha Ramesh <thanusharamesh.25@gmail.com>
2021-03-28 10:44:41 +0200

    image changes

--- src/Components/collection-item.component.jsx
+++ src/Components/collection-item.component.jsx
@@ -20,6 +20,7 @@ const CollectionItem = ({ item, addItem }) => {
       <div className='collection-footer'>
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
+        <span className='euro'>€</span>
       </div>
       <CustomButton onClick={() => addItem(item)} inverted>
         Add to cart
--- src/redux/shop/shop.data.js
+++ src/redux/shop/shop.data.js
@@ -1,246 +1,281 @@
+import Apples from '../../Images/FarmProduce/Apples.jpg'
+import Oranges from '../../Images/FarmProduce/Oranges.jpg'
+import Bellpeppers from '../../Images/FarmProduce/Bellpeppers.jpg'
+import Broccoli from '../../Images/FarmProduce/Broccoli.jpg'
+import Carrots from '../../Images/FarmProduce/Carrots.jpg'
+import Grapes from '../../Images/FarmProduce/Grapes.jpg'
+import Kiwi from '../../Images/FarmProduce/Kiwi.jpg'
+import Potatoes from '../../Images/FarmProduce/Potatoes.jpg'
+import Cauliflower from '../../Images/FarmProduce/Cauliflower.jpg'
+import Chillies from '../../Images/FarmProduce/Chillies.jpg'
+import Straberries from '../../Images/FarmProduce/Straberries.jpg'
+import Tomatoes from '../../Images/FarmProduce/Tomatoes.jpg'
+import Bread from '../../Images/Bread/Bread.jpg'
+import Corn from '../../Images/Bread/Corn.jpg'
+import Tofu from '../../Images/Bread/Tofu.jpg'
+import Bean from '../../Images/Bread/KidneyBean.jpg'
+import Lentils from '../../Images/Bread/Lentils.jpg'
+import Muffin from '../../Images/Bread/Muffinn.jpg'
+import Pasta from '../../Images/Bread/Pasta.jpg'
+import Rice from '../../Images/Bread/Rice.jpg'
+import Milk from '../../Images/Dairy/Milk.jpg'
+import Cheese from '../../Images/Dairy/Cheese.jpg'
+import Eggs from '../../Images/Dairy/Eggs.jpg'
+import Butter from '../../Images/Dairy/Butter.jpg'
+import Beef from '../../Images/Meat/Beef.jpg'
+import Fish from '../../Images/Meat/Fish.jpg'
+import Prawns from '../../Images/Meat/prawns.jpg'
+import Lamm from '../../Images/Meat/Lamm.jpg'
+
 const SHOP_DATA = {
-  hats: {
+  farm: {
     id: 1,
-    title: 'Hats',
-    routeName: 'hats',
+    title: 'Farm',
+    routeName: 'farm',
     items: [
       {
         id: 1,
-        name: 'Brown Brim',
-        imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
-        price: 25
+        name: 'Apples',
+        imageUrl: Apples,
+        price: 1.2
       },
       {
         id: 2,
-        name: 'Blue Beanie',
-        imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
-        price: 18
+        name: 'Oranges',
+        imageUrl: Oranges,
+        price: 2.1
       },
       {
         id: 3,
-        name: 'Brown Cowboy',
-        imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
-        price: 35
+        name: 'Cauliflower',
+        imageUrl: Cauliflower,
+        price: 1
       },
       {
         id: 4,
-        name: 'Grey Brim',
-        imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
-        price: 25
+        name: 'Carrots',
+        imageUrl: Carrots,
+        price: 2
       },
       {
         id: 5,
-        name: 'Green Beanie',
-        imageUrl: 'https://i.ibb.co/YTjW3vF/green-beanie.png',
-        price: 18
+        name: 'Chillies',
+        imageUrl: Chillies,
+        price: 1.2
       },
       {
         id: 6,
-        name: 'Palm Tree Cap',
-        imageUrl: 'https://i.ibb.co/rKBDvJX/palm-tree-cap.png',
-        price: 14
+        name: 'Bellpeppers',
+        imageUrl: Bellpeppers,
+        price: 1.4
       },
       {
         id: 7,
-        name: 'Red Beanie',
-        imageUrl: 'https://i.ibb.co/bLB646Z/red-beanie.png',
-        price: 18
+        name: 'Broccoli',
+        imageUrl: Broccoli,
+        price: 1.8
       },
       {
         id: 8,
-        name: 'Wolf Cap',
-        imageUrl: 'https://i.ibb.co/1f2nWMM/wolf-cap.png',
-        price: 14
+        name: 'Grapes',
+        imageUrl: Grapes,
+        price: 1.4
       },
       {
         id: 9,
-        name: 'Blue Snapback',
-        imageUrl: 'https://i.ibb.co/X2VJP2W/blue-snapback.png',
-        price: 16
+        name: 'Tomatoes',
+        imageUrl: Tomatoes,
+        price: 1.6
+      },
+	  {
+        id: 10,
+        name: 'Straberries',
+        imageUrl: Straberries,
+        price: 2.9
+      },
+	   {
+        id: 11,
+        name: 'Kiwi',
+        imageUrl: Kiwi,
+        price: 2.3
+      },
+	   {
+        id: 12,
+        name: 'Potatoes',
+        imageUrl: Potatoes,
+        price: 1.7
       }
     ]
   },
-  sneakers: {
+  condiments: {
     id: 2,
-    title: 'Sneakers',
-    routeName: 'sneakers',
+    title: 'Condiments',
+    routeName: 'condiments',
     items: [
       {
-        id: 10,
-        name: 'Adidas NMD',
-        imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
-        price: 220
-      },
-      {
-        id: 11,
-        name: 'Adidas Yeezy',
-        imageUrl: 'https://i.ibb.co/dJbG1cT/yeezy.png',
-        price: 280
-      },
-      {
-        id: 12,
-        name: 'Black Converse',
-        imageUrl: 'https://i.ibb.co/bPmVXyP/black-converse.png',
-        price: 110
-      },
-      {
         id: 13,
-        name: 'Nike White AirForce',
-        imageUrl: 'https://i.ibb.co/1RcFPk0/white-nike-high-tops.png',
-        price: 160
+        name: 'Bread',
+        imageUrl: Bread,
+        price: 2.2
       },
       {
         id: 14,
-        name: 'Nike Red High Tops',
-        imageUrl: 'https://i.ibb.co/QcvzydB/nikes-red.png',
-        price: 160
+        name: 'Corn',
+        imageUrl: Corn,
+        price: 2.80
       },
       {
         id: 15,
-        name: 'Nike Brown High Tops',
-        imageUrl: 'https://i.ibb.co/fMTV342/nike-brown.png',
-        price: 160
+        name: 'Tofu',
+        imageUrl: Tofu,
+        price: 1.10
       },
       {
         id: 16,
-        name: 'Air Jordan Limited',
-        imageUrl: 'https://i.ibb.co/w4k6Ws9/nike-funky.png',
-        price: 190
+        name: 'Bean',
+        imageUrl: Bean,
+        price: 1.60
       },
       {
         id: 17,
-        name: 'Timberlands',
-        imageUrl: 'https://i.ibb.co/Mhh6wBg/timberlands.png',
-        price: 200
-      }
-    ]
-  },
-  jackets: {
-    id: 3,
-    title: 'Jackets',
-    routeName: 'jackets',
-    items: [
+        name: 'Lentils',
+        imageUrl: Lentils,
+        price: 1.60
+      },
       {
         id: 18,
-        name: 'Black Jean Shearling',
-        imageUrl: 'https://i.ibb.co/XzcwL5s/black-shearling.png',
-        price: 125
+        name: 'Muffin',
+        imageUrl: Muffin,
+        price: 1.90
       },
       {
         id: 19,
-        name: 'Blue Jean Jacket',
-        imageUrl: 'https://i.ibb.co/mJS6vz0/blue-jean-jacket.png',
-        price: 90
+        name: 'Pasta',
+        imageUrl: Pasta,
+        price: 1.90
       },
       {
         id: 20,
-        name: 'Grey Jean Jacket',
-        imageUrl: 'https://i.ibb.co/N71k1ML/grey-jean-jacket.png',
-        price: 90
-      },
+        name: 'Rice',
+        imageUrl: Rice,
+        price: 2.00
+      }
+    ]
+  },
+  bread: {
+    id: 3,
+    title: 'Bread',
+    routeName: 'bread',
+    items: [
       {
         id: 21,
-        name: 'Brown Shearling',
-        imageUrl: 'https://i.ibb.co/s96FpdP/brown-shearling.png',
-        price: 165
+        name: 'Bread',
+        imageUrl: Bread,
+        price: 2.20
       },
       {
         id: 22,
-        name: 'Tan Trench',
-        imageUrl: 'https://i.ibb.co/M6hHc3F/brown-trench.png',
-        price: 185
-      }
-    ]
-  },
-  womens: {
-    id: 4,
-    title: 'Womens',
-    routeName: 'womens',
-    items: [
+        name: 'Corn',
+        imageUrl: Corn,
+        price: 2.80
+      },
       {
         id: 23,
-        name: 'Blue Tanktop',
-        imageUrl: 'https://i.ibb.co/7CQVJNm/blue-tank.png',
-        price: 25
+        name: 'Tofu',
+        imageUrl: Tofu,
+        price: 1.10
       },
       {
         id: 24,
-        name: 'Floral Blouse',
-        imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
-        price: 20
+        name: 'Bean',
+        imageUrl: Bean,
+        price: 1.60
       },
       {
         id: 25,
-        name: 'Floral Dress',
-        imageUrl: 'https://i.ibb.co/KV18Ysr/floral-skirt.png',
-        price: 80
+        name: 'Lentils',
+        imageUrl: Lentils,
+        price: 1.60
       },
       {
         id: 26,
-        name: 'Red Dots Dress',
-        imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
-        price: 80
+        name: 'Muffin',
+        imageUrl: Muffin,
+        price: 1.60
       },
       {
         id: 27,
-        name: 'Striped Sweater',
-        imageUrl: 'https://i.ibb.co/KmSkMbH/striped-sweater.png',
-        price: 45
+        name: 'Pasta',
+        imageUrl: Pasta,
+        price: 1.90
       },
       {
         id: 28,
-        name: 'Yellow Track Suit',
-        imageUrl: 'https://i.ibb.co/v1cvwNf/yellow-track-suit.png',
-        price: 135
-      },
-      {
-        id: 29,
-        name: 'White Blouse',
-        imageUrl: 'https://i.ibb.co/qBcrsJg/white-vest.png',
-        price: 20
+        name: 'Rice',
+        imageUrl: Rice,
+        price: 2.00
       }
     ]
   },
-  mens: {
+  meat: {
+    id: 4,
+    title: 'Meat',
+    routeName: 'meat',
+    items: [
+      {
+        id: 23,
+        name: 'Beef',
+        imageUrl: Beef,
+        price: 2.5
+      },
+      {
+        id: 24,
+        name: 'Fish',
+        imageUrl: Fish,
+        price: 2.20
+      },
+      {
+        id: 25,
+        name: 'Prawns',
+        imageUrl: Prawns,
+        price: 3.0
+      },
+      {
+        id: 26,
+        name: 'Lamm',
+        imageUrl: Lamm,
+        price: 5.0
+      }      
+    ]
+  },
+  dairy: {
     id: 5,
-    title: 'Mens',
-    routeName: 'mens',
+    title: 'Dairy',
+    routeName: 'dairy',
     items: [
       {
         id: 30,
-        name: 'Camo Down Vest',
-        imageUrl: 'https://i.ibb.co/xJS0T3Y/camo-vest.png',
-        price: 325
+        name: 'Milk',
+        imageUrl: Milk,
+        price: 2.5
       },
       {
         id: 31,
-        name: 'Floral T-shirt',
-        imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
-        price: 20
+        name: 'Eggs',
+        imageUrl: Eggs,
+        price: 2.80
       },
       {
         id: 32,
-        name: 'Black & White Longsleeve',
-        imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
-        price: 25
+        name: 'Cheese',
+        imageUrl: Cheese,
+        price: 2.5
       },
       {
         id: 33,
-        name: 'Pink T-shirt',
-        imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
-        price: 25
-      },
-      {
-        id: 34,
-        name: 'Jean Long Sleeve',
-        imageUrl: 'https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png',
-        price: 40
-      },
-      {
-        id: 35,
-        name: 'Burgundy T-shirt',
-        imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
-        price: 25
+        name: 'Butter',
+        imageUrl: Butter,
+        price: 2.5
       }
     ]
   }
